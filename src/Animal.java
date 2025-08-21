057ce661e3e08bdf53ad5ab87506b48450f49424
Anshul <mr.anshuking@gmail.com>
2025-08-21 10:13:48 +0530

    Student

---
+++ src/Animal.java
@@ -0,0 +1,23 @@
+public class Animal {
+    String color;
+    int age;
+    void initObj(String c,int a){
+        color=c;
+        age=a;
+
+
+    }
+    public void eat(){
+        System.out.print("eating RN babes");
+    }
+    public static void main(String[] args){
+        Animal buzo=new Animal();
+        buzo.initObj("Brown and White",12);
+        buzo.display();
+    }
+    public void display(){
+        System.out.println("His colour is "+color+" and he is "+age+" old");
+    }
+
+
+}

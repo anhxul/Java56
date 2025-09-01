7a71b319a80c9503f9a216252dc7dafd280df74f
Anshul <mr.anshuking@gmail.com>
2025-09-01 14:59:48 +0530

    One-D array

---
+++ src/Array1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Array1 {
+
+    public static void main(String[] args) {
+        int a[]=new int[5];
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the elements you want to add to the array:");
+
+        for(int i=0;i<a.length;i++){
+            a[i]=sc.nextInt();
+        }
+        System.out.println("Array elements are : ");
+        for(int i=0;i<a.length;i++){
+            System.out.println("Element at "+i+" Postition is "+a[i]);
+
+        }
+    }
+}

85337d165c733b849b00d66e334c06c2f4bdfed3
Anshul <mr.anshuking@gmail.com>
2025-09-25 09:55:29 +0530

    THREAD 25/09/25

--- src/UNIT_2/Thread1Main.java
+++ src/UNIT_2/Thread1Main.java
@@ -3,6 +3,8 @@ package UNIT_2;
 public class Thread1Main {
     public static void main(String[] args) {
         Thread1 th1=new Thread1();
+        Thread2 th2=new Thread2();
         th1.start();
+        th2.start();
     }
 }
--- src/UNIT_2/Thread2.java
+++ src/UNIT_2/Thread2.java
@@ -1,24 +1,16 @@
 package UNIT_2;
 
-public class THREAD2 extends Thread{
-    public void run()
-    {
+public class Thread2 extends Thread {
+    public void run() {
         try {
             for (int i = 1; i <= 100; i++) {
                 if (i % 2 != 0) {
-                    System.out.println(i);
+                    System.out.println("Thread 2...Odd numbers : "+i);
                     Thread.sleep(2000);
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-            catch(Exception e){
-                e.printStackTrace();
-            }
-
-
-        }
-
-
     }
 }
